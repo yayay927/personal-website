00d4d45d0010dd30b5124640518dad469cba5806
yayay927 <isabelleya927@gmail.com>
2024-09-12 16:51:53 +0800

    create next-react app

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,8 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li className="mb-2">Save and see your changes instantly.</li>
+          <li>And have fun.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
